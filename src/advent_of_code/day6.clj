8cf4742709c028c7bcf67dc58a5e65799ac43dab
Guenter Kickinger <kixi73@gmail.com>
2017-12-06 17:44:16 +0100

    not finished

--- src/advent_of_code/day6.clj
+++ src/advent_of_code/day6.clj
@@ -2,3 +2,21 @@
   (:require [clojure.test :refer [is deftest]]
             [clojure.string :as str]))
 
+(def b [0 2 7 0])
+(defn highest-bank [banks]
+  (reduce (fn [[idxa vala] [idx val]]
+            (if (> val vala)
+              [idx val]
+              [idxa vala]))
+          (map vector (range 0 (count banks)) banks )))
+
+(highest-bank b)
+
+(defn distribute [banks]
+  (let [[idx val] (highest-bank banks)
+        rem-banks (assoc banks idx 0)]
+
+    rem-banks
+    ))
+
+(distribute b)
