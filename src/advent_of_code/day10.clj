f8f22ca7f0edafd5116ca09041b8d8e1b974c7d8
Guenter Kickinger <kixi73@gmail.com>
2017-12-12 20:54:19 +0100

    Day 10

--- src/advent_of_code/day10.clj
+++ src/advent_of_code/day10.clj
@@ -14,38 +14,34 @@
      :pos (mod (+ pos skip len) (count hash))})
   )
 
-(defn position)
-
-(clojure.pprint/pprint (reductions
-                        hash-step
-                        {:hash list-of-number
-                         :pos 0}
-                        (map vector  [3 4 1 5] (range 0 5))))
-
-
-(println (hash-step {:hash [4 3 0 1 2] :pos 1} [5 3]))
 
 (def input [183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88])
-(let [hash (reduce
+
+(defn round [input]
+  (reduce
             hash-step
             {:hash (range 0 256)
              :pos 0}
-            (map vector input (range 0 (inc (count input)) )))]
-  (* (first (:hash hash)) (second (:hash hash))))
+            (map vector input (iterate inc 0))))
+
+
+(defn part-1 []
+  (let [hash (round input)]
+    (* (first (:hash hash)) (second (:hash hash)))))
+
+(part-1)
 
 (def input-str "183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88")
+
 (defn ascii [input]
   (concat (map int input) [17,31,73,47,23]))
 
-(def asc (ascii input-str))
-
-(defn sparse-hash [asc]
+(defn sparse-hash [input]
   (let [hash (reduce
               hash-step
               {:hash (range 0 256)
                :pos 0}
-              (->> (map vector asc (range 0 (inc (count asc)) ))
-                   (mapcat #(repeat 64 %)))
+              (map vector (flatten (repeat 64 input)) (iterate inc 0)) 
               )]
     (:hash hash)
     ))
@@ -58,14 +54,8 @@
 
 (defn hash-f [input]
   (-> (ascii input)
-      println
       sparse-hash
-      println
       dense-hash
-      println
       ))
 
 (hash-f input-str)
-(hash-f "")
-(dense-hash sp-hash)
-(format "%02x" 47)
